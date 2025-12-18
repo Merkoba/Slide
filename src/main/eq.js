b067cf4983dc5b0e5cd580948fad4364c5f27646
Auric Vente <mprops@protonmail.com>
2025-12-18 12:37:10 -0600

    Mods

--- src/main/eq.js
+++ src/main/eq.js
@@ -1,3 +1,54 @@
+App.eq_range = 12
+
 App.setup_eq = () => {
   let low = DOM.el(`#eq-low`)
+  let mid = DOM.el(`#eq-mid`)
+  let high = DOM.el(`#eq-high`)
+
+  function apply_eq() {
+    let low_val = parseInt(low.value)
+    let mid_val = parseInt(mid.value)
+    let high_val = parseInt(high.value)
+    App.set_eq(low_val, mid_val, high_val)
+  }
+
+  function increase(el) {
+    let value = parseInt(el.value)
+    el.value = Math.min(App.eq_range, value + 1)
+    apply_eq()
+  }
+
+  function decrease(el) {
+    let value = parseInt(el.value)
+    el.value = Math.max(-App.eq_range, value - 1)
+    apply_eq()
+  }
+
+  function register(what) {
+    DOM.ev(what, `change`, () => {
+      apply_eq()
+    })
+
+    DOM.ev(what, `auxclick`, (event) => {
+      if (event.button === 1) {
+        event.target.value = 0
+        apply_eq()
+      }
+
+      event.preventDefault()
+    })
+
+    DOM.ev(what, `wheel`, (event) => {
+      if (event.deltaY < 0) {
+        increase(event.target)
+      }
+      else if (event.deltaY > 0) {
+        decrease(event.target)
+      }
+    })
+  }
+
+  register(low)
+  register(mid)
+  register(high)
 }
