a1b1141379ee2760d11128539aeaf7241e42503d
Auric Vente <mprops@protonmail.com>
2025-12-18 14:27:38 -0600

    Mods

--- src/main/tooltips.js
+++ src/main/tooltips.js
@@ -58,7 +58,7 @@ App.show_tooltip = (e) => {
   App.tooltip_el.textContent = text
   App.tooltip_el.style.display = `block`
 
-  let { top, left } = App.get_tooltip_position(e.target)
+  let {top, left} = App.get_tooltip_position(e.target)
 
   App.tooltip_el.style.top = `${top}px`
   App.tooltip_el.style.left = `${left}px`
